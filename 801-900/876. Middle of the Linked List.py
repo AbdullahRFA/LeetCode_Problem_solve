464801882823e808483179e7bde684b6b974af00
AbdullahRFA <shakibrybmn@gmail.com>
2025-08-02 11:50:14 +0600

    added a problem that find the middle of linked list

---
+++ 801-900/876. Middle of the Linked List.py
@@ -0,0 +1,40 @@
+"""
+Given the head of a singly linked list, return the middle node of the linked list.
+
+If there are two middle nodes, return the second middle node.
+
+
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5]
+Output: [3,4,5]
+Explanation: The middle node of the list is node 3.
+Example 2:
+
+
+Input: head = [1,2,3,4,5,6]
+Output: [4,5,6]
+Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+"""
+from typing import Optional
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        current = head
+        count = 0
+        while current:
+            current = current.next
+            count += 1
+        mid = count // 2
+        mid_node = head
+        for _ in range(mid):
+            mid_node = mid_node.next
+        return mid_node
