c863939836b4229295918e9299d798cddbcbc3f0
AbdullahRFA <shakibrybmn@gmail.com>
2026-05-06 13:26:24 +0600

    solve nth digit using math+group

---
+++ 301-400/400. Nth Digit.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findNthDigit(self, n: int) -> int:
+        digit_length = 1
+        count = 9
+        start = 1
+
+        while n> count*digit_length:
+            n-=count*digit_length
+            count*=10
+            start*=10
+            digit_length +=1
+        
+        number = start+(n-1)//digit_length
+        idx = (n-1)%digit_length
+
+        return int(str(number)[idx])
